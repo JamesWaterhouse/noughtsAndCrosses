a79a4858f1f28970be985220fcb2420e4beda438
James Waterhouse <james.waterhouse@yahoo.co.uk>
2020-06-13 11:05:05 +0100

    working with Xwin Owin Draw Initial InPlay

--- Program.cs
+++ Program.cs
@@ -7,14 +7,29 @@ namespace noughtsAndCrosses
         private enum BoardState 
         {
             // complete this enum with all the possible states of a noughts and crosses board (there's more than 3)
-            INITIAL, IN_PLAY, NOUGHTS_WIN, CROSSES_WIN, DRAW, WIN
+            INITIAL, IN_PLAY, NOUGHTS_WIN, CROSSES_WIN, DRAW, INVALID
         }
+
+        private static BoardState CheckWinner(char a)
+            {
+                if(a == 'X') 
+                {
+                    Console.WriteLine("Crosses Win!");
+                   return BoardState.CROSSES_WIN;
+                }
+                else
+                {
+                    Console.WriteLine("Noughts Win!");
+                    return BoardState.NOUGHTS_WIN;
+                }
+            }
+
         private static BoardState GetStateOfBoard(string board) 
         {
             // complete this method so that it returns the correct board state
             if(board == "_________") 
             {
-                Console.WriteLine("The game has not yet started");
+                Console.WriteLine("The game is ready to start");
                 return BoardState.INITIAL;
             }
           
@@ -28,15 +43,42 @@ namespace noughtsAndCrosses
             char h = board[7];
             char i = board[8];
 
-            // if((a.Equals(b) && a.Equals(c)) || (d.Equals(e) && d.Equals(f)) || (g.Equals(h) && g.Equals(i)) || (a.Equals(d) && a.Equals(g)) || (b.Equals(e) && b.Equals(h)) || (c.Equals(f) && c.Equals(i)) || (a.Equals(e) && a.Equals(i)) || (g.Equals(e) && g.Equals(c))) {
+        if(((a.Equals(b) && a.Equals(c)) || (a.Equals(d) && a.Equals(g)) || (a.Equals(e) && a.Equals(i))) && !(a.Equals('_')))
+            {   
+                Console.WriteLine(a);
+                Console.WriteLine("a ran");
+                return CheckWinner(a);  
+            }
 
-            //     return BoardState.WIN;
-            // }
+            else if (((g.Equals(h) && g.Equals(i)) || (c.Equals(f) && c.Equals(i))) && !(i.Equals('_'))) 
+            {
+                Console.WriteLine("i ran");
+                return CheckWinner(i); 
+            }
 
+            else if(d.Equals(e) && d.Equals(f) && !(d.Equals('_'))) 
+            {
+                Console.WriteLine("d ran");
+                return CheckWinner(d); 
+            }
 
-           
-          
+            else if(b.Equals(e) && b.Equals(h) && !(b.Equals('_'))) 
+            {
+                Console.WriteLine("b ran");
+                return CheckWinner(b); 
+            }
 
+            else if(g.Equals(e) && g.Equals(c) && !(g.Equals('_'))) 
+            {
+                Console.WriteLine("g ran");
+                return CheckWinner(g); 
+            }
+
+            if(board.Contains('_')) {
+                Console.WriteLine("There are moves left to play");
+                return BoardState.IN_PLAY;
+            }
+            Console.WriteLine("It's a draw!");
             return BoardState.DRAW;
         }
         static void Main(string[] args) 
