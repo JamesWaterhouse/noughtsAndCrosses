72f7a9acb40539700fbe1cf25c65c539ce9402ca
James Waterhouse <james.waterhouse@yahoo.co.uk>
2020-06-13 11:21:33 +0100

    working

--- Program.cs
+++ Program.cs
@@ -28,53 +28,62 @@ namespace noughtsAndCrosses
         {
             // complete this method so that it returns the correct board state
             
-            char a = board[0];
-            char b = board[1];
-            char c = board[2];
-            char d = board[3];
-            char e = board[4];
-            char f = board[5];
-            char g = board[6];
-            char h = board[7];
-            char i = board[8];
-
-            if(board == "_________") 
+            if(board.Length == 9) 
             {
-                Console.WriteLine("The game is ready to start");
-                return BoardState.INITIAL;
+
+                char a = board[0];
+                char b = board[1];
+                char c = board[2];
+                char d = board[3];
+                char e = board[4];
+                char f = board[5];
+                char g = board[6];
+                char h = board[7];
+                char i = board[8];
+
+                if(board == "_________") 
+                {
+                    Console.WriteLine("The game is ready to start");
+                    return BoardState.INITIAL;
+                }
+
+                if(((a.Equals(b, c) && a.Equals(c)) || (a.Equals(d) && a.Equals(g)) || (a.Equals(e) && a.Equals(i))) && !(a.Equals('_')))
+                {   
+                    return CheckWinner(a);  
+                }
+
+                else if (((g.Equals(h) && g.Equals(i)) || (c.Equals(f) && c.Equals(i))) && !(i.Equals('_'))) 
+                {
+                    return CheckWinner(i); 
+                }
+
+                else if(d.Equals(e) && d.Equals(f) && !(d.Equals('_'))) 
+                {
+                    return CheckWinner(d); 
+                }
+
+                else if(b.Equals(e) && b.Equals(h) && !(b.Equals('_'))) 
+                {
+                    return CheckWinner(b); 
+                }
+
+                else if(g.Equals(e) && g.Equals(c) && !(g.Equals('_'))) 
+                {
+                    return CheckWinner(g); 
+                }
+
+                if(board.Contains('_')) 
+                {
+                    Console.WriteLine("There are moves left to play");
+                    return BoardState.IN_PLAY;
+                }
+
+                Console.WriteLine("It's a draw!");
+                return BoardState.DRAW;   
             }
 
-            if(((a.Equals(b) && a.Equals(c)) || (a.Equals(d) && a.Equals(g)) || (a.Equals(e) && a.Equals(i))) && !(a.Equals('_')))
-            {   
-                return CheckWinner(a);  
-            }
-
-            else if (((g.Equals(h) && g.Equals(i)) || (c.Equals(f) && c.Equals(i))) && !(i.Equals('_'))) 
-            {
-                return CheckWinner(i); 
-            }
-
-            else if(d.Equals(e) && d.Equals(f) && !(d.Equals('_'))) 
-            {
-                return CheckWinner(d); 
-            }
-
-            else if(b.Equals(e) && b.Equals(h) && !(b.Equals('_'))) 
-            {
-                return CheckWinner(b); 
-            }
-
-            else if(g.Equals(e) && g.Equals(c) && !(g.Equals('_'))) 
-            {
-                return CheckWinner(g); 
-            }
-
-            if(board.Contains('_')) {
-                Console.WriteLine("There are moves left to play");
-                return BoardState.IN_PLAY;
-            }
-            Console.WriteLine("It's a draw!");
-            return BoardState.DRAW;
+            Console.WriteLine("The board must be made up of 9 squares");
+            return BoardState.INVALID;
         }
         static void Main(string[] args) 
         {
